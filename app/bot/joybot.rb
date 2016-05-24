853619b90b540f3ea298b3f5d9337f757daebadc
Dev Chakraborty <devchakraborty@users.noreply.github.com>
2016-05-24 17:42:04 -0400

    Add message YAML format + refactor Joybot

--- app/bot/joybot.rb
+++ app/bot/joybot.rb
@@ -1,17 +1,14 @@
 class Joybot
   include Facebook::Messenger
+
+  ACTIONS = [:get_started, :menu]
+
   def initialize
     Facebook::Messenger.config.access_token = ENV['FB_ACCESS_TOKEN']
     Facebook::Messenger.config.verify_token = ENV['FB_VERIFY_TOKEN']
 
     @graph = Koala::Facebook::API.new(ENV['FB_ACCESS_TOKEN'])
-    @wit = Wit.new(ENV['WIT_ACCESS_TOKEN'], {
-        say: -> (session_id, context, msg) { self.say(session_id, context, msg) },
-        merge: -> (session_id, context, entities, msg) { self.merge(session_id, context, entities, msg) },
-        opt_in: -> (session_id, context) { self.opt_in(session_id, context) },
-        get_started: -> (session_id, context) { self.get_started(session_id, context) },
-        error: -> (session_id, context, error) { self.error(session_id, context, error) },
-    })
+    @wit = Wit.new(ENV['WIT_ACCESS_TOKEN'], wit_actions)
 
     Bot.on :message do |message|
       sender = message.sender['id']
@@ -20,6 +17,9 @@ class Joybot
       Rails.logger.info "[ #{sender} @ #{at} ] >> #{text}"
       wit.run_actions(sender, text, {})
     end
+
+    @config = YAML.load(File.read(Rails.root.to_s + "/config/joybot_config.yml"))[Rails.env].deep_symbolize_keys!
+    @responses = config[:responses]
   end
 
   def say(session_id, context, msg)
@@ -28,10 +28,34 @@ class Joybot
         id: session_id
       },
       message: {
-        text: msg
+        text: Erubis::Eruby.new(msg).result(context)
       }
     )
     return context
+  end
+
+  def say_with_buttons(session_id, context, msg, buttons)
+    Bot.deliver(
+      recipient: {
+        id: session_id
+      },
+      message: {
+        attachment: {
+          type: 'template',
+          payload: {
+            template_type: 'button',
+            text: Erubis::Eruby.new(msg).result(context),
+            buttons: buttons.map { |button_label, button_text|
+              {
+                type: 'postback',
+                title: button_text,
+                payload: button_label
+              }
+            }
+          }
+        }
+      }
+    )
   end
 
   def merge(session_id, received_context, entities, msg)
@@ -51,7 +75,7 @@ class Joybot
     }
   end
 
-  def get_started_first_time(session_id, received_context)
+  def get_started(session_id, received_context)
     inject_context(session_id, received_context) { |context|
       Bot.deliver(
         recipient: {
@@ -76,12 +100,47 @@ class Joybot
 
   def error(session_id, received_context, error)
     inject_context(session_id, received_context) { |context|
-      p context
+      p error, context
     }
   end
 
   private
-  attr_accessor :graph, :wit
+  attr_accessor :graph, :wit, :config, :responses
+
+  def wit_actions
+    @wit_actions ||= begin
+      actions = {
+        say: -> (session_id, context, msg) { self.say(session_id, context, msg) },
+        merge: -> (session_id, context, entities, msg) { self.merge(session_id, context, entities, msg) },
+        error: -> (session_id, context, error) { self.error(session_id, context, error) },
+      }
+
+      ACTIONS.each do |action|
+        actions[action] = -> (session_id, context) {
+          process_action(action, session_id, context)
+        }
+      end
+
+      actions
+    end
+  end
+
+  def process_action(action, fb_id, received_context)
+    inject_context(fb_id, received_context) do |context|
+      response = responses[action]
+
+      response.each do |message|
+        # TODO: Add other message types
+        message_text = message[:text]
+        message_buttons = message[:buttons]
+        if message_buttons.nil?
+          say(fb_id, context, message[:text])
+        else
+          say_with_buttons(fb_id, context, message[:text], message[:buttons])
+        end
+      end
+    end
+  end
 
   def inject_context(fb_id, context)
     user = User.find_by(fb_id: fb_id)
@@ -89,6 +148,7 @@ class Joybot
       user = create_user(fb_id)
     end
     new_context = user.context.merge(context)
+    new_context.merge!(no_memories: true) # TODO: Use actual records
     yield(new_context)
     unless user.context == new_context
       user.update(context: new_context)
